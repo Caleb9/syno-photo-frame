a947983ceafa7134333b62502ade11779a6b6259
Piotr Karasinski <7767011+Caleb9@users.noreply.github.com>
2023-05-16 13:46:42 +0200

    Handle ctrl-c and other termination signals

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, sync::Arc};
+use std::{error::Error, process, sync::Arc};
 
 use clap::Parser;
 use reqwest::{blocking::ClientBuilder, cookie::CookieStore};
@@ -10,6 +10,8 @@ use syno_photo_frame::{
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
+    ctrlc::set_handler(|| process::exit(0))?;
+
     let cli = Cli::parse();
     let cookie_store = Arc::new(reqwest::cookie::Jar::default());
     let client = ClientBuilder::new()
